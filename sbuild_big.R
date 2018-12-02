07e2f3f7be198788022f9bca4d7129f239a99435
moshe-olshansky <40348083+moshe-olshansky@users.noreply.github.com>
2018-12-02 13:26:31 +1100

    Add files via upload

---
+++ sbuild_big.R
@@ -0,0 +1,28 @@
+Sys.setenv("PKG_CXXFLAGS"="-std=c++11")
+library(Rcpp)
+sourceCpp("~/HiC/straw/R/straw-R.cpp")
+
+options(stringsAsFactors=FALSE)
+options(scipen=10)
+
+hicfile <-"/home/mols0001/HiC/GM12878/GM12878_insitu_combined_30.hic"
+binsize <- 5000
+x <- read.table("~/HiC/hg19.chrom.sizes")
+bad <- which(x[,1] == "chrM")
+if (length(bad) > 0) x[bad,1] <- "chrMT"
+chrs <- gsub("chr","",x[,1])
+len <- x[,2]
+n <- nrow(x)
+off <- rep(0,n)
+off[1] <- 1
+for (i in 1:(n-1)) off[i+1] <- off[i] + ceiling(len[i]/binsize)
+
+con <- file("new_big_5K.h5","w")
+for (i in 1:n) for (j in i:n) {
+	x <- straw_R(paste("NONE",hicfile,chrs[i],chrs[j],"BP",binsize))
+	x$x <- off[i] + floor(x$x/binsize)
+	x$y <- off[j] + floor(x$y/binsize)
+	write.table(x,con,append=TRUE,quote=FALSE,col.names=FALSE,row.names=FALSE)
+	print(paste(chrs[i],chrs[j]))
+}
+close(con)
